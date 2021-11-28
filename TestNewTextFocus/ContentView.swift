11fea6d2c348223144198e2d2513609af4d3514e
David Siebecker <david.siebecker@gmail.com>
2021-11-28 15:31:29 -0500

    focus follows using environmentobject

--- TestNewTextFocus/ContentView.swift
+++ TestNewTextFocus/ContentView.swift
@@ -7,10 +7,66 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FocusedTextEditorKey: EnvironmentKey {
+    static let defaultValue: UUID = UUID()
+}
+
+extension EnvironmentValues {
+    var focusedTextEditor : UUID {
+        get {self[FocusedTextEditorKey.self]}
+        set {self[FocusedTextEditorKey.self] = newValue}
+    }
+}
+
+
+class Person: ObservableObject, Identifiable {
+    
+    @Published var name: String = ""
+    let id = UUID()
+
+}
+
+class FocusedEditor : ObservableObject {
+    
+    @Published var focusedTextEditor: UUID?
+    init(_ uuid: UUID) {
+        focusedTextEditor = uuid
+    }
+}
+
+struct TextFieldWrapper : View {
+    @ObservedObject var person: Person
+    @EnvironmentObject var focusedWrapper: FocusedEditor
+    @FocusState private var focused: UUID?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TextField("Name",text: $person.name)
+            .focused($focused,equals: focusedWrapper.focusedTextEditor)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                    focused = person.id
+                }
+            }
+    }
+}
+
+struct ContentView: View {
+    @State var people = [Person]()
+    @State var firstResponder = FocusedEditor(UUID())
+    
+    var body: some View {
+        List {
+            Button("Add Person") {
+                people.append(Person())
+                if let id = people.last?.id {
+                    firstResponder.focusedTextEditor = id
+                }
+            }
+            ForEach(people) {person in
+                TextFieldWrapper(person: person)
+            }.environmentObject(firstResponder)
+        }
+        
     }
 }
 
