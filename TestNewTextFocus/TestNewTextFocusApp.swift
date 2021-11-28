41ed651cfa00947bc66f2ef99347acc50b97bb8b
David Siebecker <david.siebecker@gmail.com>
2021-11-28 13:04:42 -0500

    Initial Commit

---
+++ TestNewTextFocus/TestNewTextFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNewTextFocusApp.swift
+//  TestNewTextFocus
+//
+//  Created by David Siebecker on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TestNewTextFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
